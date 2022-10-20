b0891b897dbe58a53848300246d064d6386f2d12
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2022-10-20 07:29:46 +0700

    Complete exercise Conditional JS

---
+++ Exercise/Conditional/js-advance-conditional-cp-1-v2/main.js
@@ -0,0 +1,34 @@
+function rockPaperScissor(player1, player2) {
+  if (player1 === "scissor") {
+    if (player2 === "paper") {
+      return `Player 1 Won`;
+    } else if (player1 === player2) {
+      return `draw`;
+    } else if (player2 === "rock") {
+      return `Player 2 Won`;
+    }
+  } else if (player1 == "rock") {
+    if (player2 === "paper") {
+      return `Player 2 Won`;
+    } else if (player1 === player2) {
+      return `draw`;
+    } else if (player2 === "scissor") {
+      return `Player 1 Won`;
+    }
+  } else if (player1 == "paper") {
+    if (player2 === "scissor") {
+      return `Player 2 Won`;
+    } else if (player1 === player2) {
+      return `draw`;
+    } else if (player2 === "rock") {
+      return `Player 1 Won`;
+    }
+  }
+}
+
+console.log(rockPaperScissor("scissor", "paper")); // "Player 1 Won!"
+console.log(rockPaperScissor("rock", "paper")); // "Player 2 Won!"
+console.log(rockPaperScissor("paper", "paper")); // "Draw!"
+console.log(rockPaperScissor("rock", "rock")); // "Draw!"
+
+module.exports = rockPaperScissor;
